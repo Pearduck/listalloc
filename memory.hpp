5b80a5815a3520a8074174ce31d8e5b84af43336
Pearduck <selfdestruct@bk.ru>
2018-05-11 17:56:23 +0300

    add unique_ptr

--- memory.hpp
+++ memory.hpp
@@ -4,12 +4,32 @@
 namespace sus
 {
     template<typename T>
+    struct _deleter
+    {
+        using traits = std::allocator_traits<listAllocator<T>>;
+        void operator()(T *ptr) const
+        {
+            traits::destroy(ptr);
+            traits::deallocate(ptr, 1);
+        }
+    };
+    template<typename T>
+    using unique_ptr = std::unique_ptr<T, _deleter<T>>;
+    template<typename T>
     using shared_ptr = std::shared_ptr<T>;
     template<typename T>
     using weak_ptr = std::weak_ptr<T>;
     template<typename T, typename... Args>
-    sus::shared_ptr<T> make_shared(Args&&... args)
+    sus::shared_ptr<T> make_shared(Args &&... args)
     {
         return std::allocate_shared<T>(listAllocator<T>(), std::forward<Args>(args)...);
+    }
+    template<typename T, typename... Args>
+    sus::unique_ptr<T> make_unique(Args &&... args)
+    {
+        using traits = std::allocator_traits<listAllocator<T>>;
+        T *ptr = traits::allocate(1);
+        traits::construct(ptr, std::forward<Args>(args)...);
+        return sus::unique_ptr<T>(ptr);
     }
 };
